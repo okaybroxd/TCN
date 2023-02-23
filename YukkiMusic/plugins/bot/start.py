18a0dd2d45b889d3d9aae1a2677e891c6a55d2d5
Aditay <123898024+AdityaXoreki@users.noreply.github.com>
2023-02-23 11:34:11 +0530

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -13,7 +13,7 @@ from strings import get_command, get_string
 from YukkiMusic import Telegram, YouTube, app
 from YukkiMusic.misc import SUDOERS, _boot_
 from YukkiMusic.plugins.play.playlist import del_plist_msg
-from YukkiMusic.plugins.sudoers import sudoers_list
+from YukkiMusic.plugins.sudo.sudoers import sudoers_list
 from YukkiMusic.utils.database import (add_served_chat,
                                        add_served_user,
                                        get_served_chats,
